002fad00f9a1969b15311a194157ebae6220bf3c
Ziying Jian <73121861+zeeyingg@users.noreply.github.com>
2022-01-06 13:59:27 -0500

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -103,48 +103,4 @@ public static void walkElement(ArrayList<Comparable> data, int index) {
     } // end first loop
     }
   
-  
-  public static void main( String [] args )
-  {
-
-    //===============for VOID methods=============
-    ArrayList glen = new ArrayList<Integer>();
-    glen.add(7);
-    glen.add(1);
-    glen.add(5);
-    glen.add(12);
-    glen.add(3);
-    System.out.println( "ArrayList glen before sorting:\n" + glen );
-    selectionSortV(glen);
-    System.out.println( "ArrayList glen after sorting:\n" + glen );
-
-    ArrayList coco = populate( 10, 1, 1000 );
-    System.out.println( "ArrayList coco before sorting:\n" + coco );
-    selectionSortV(coco);
-    System.out.println( "ArrayList coco after sorting:\n" + coco );
-    // ============================================
-
-    //==========for AL-returning methods==========
-      glen = new ArrayList<Integer>();
-      glen.add(7);
-      glen.add(1);
-      glen.add(5);
-      glen.add(12);
-      glen.add(3);
-      System.out.println( "ArrayList glen before sorting:\n" + glen );
-      ArrayList glenSorted = selectionSort( glen );
-      System.out.println( "sorted version of ArrayList glen:\n"
-      + glenSorted );
-      System.out.println( "ArrayList glen after sorting:\n" + glen );
-
-      coco = populate( 10, 1, 1000 );
-      System.out.println( "ArrayList coco before sorting:\n" + coco );
-      ArrayList cocoSorted = selectionSort( coco );
-      System.out.println( "sorted version of ArrayList coco:\n"
-      + cocoSorted );
-      System.out.println( "ArrayList coco after sorting:\n" + coco );
-
-  //============================================
-
-  }//end main
 }

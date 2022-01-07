e84419aabdd426855ddd30a4302e7bd6f915f533
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2022-01-07 14:06:11 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -5,15 +5,18 @@ public class Driver{
   public static void main( String [] args )
   {
     Sorts pochmann = new Sorts();
-    //===============for VOID methods=============
+    
     ArrayList glen = new ArrayList<Integer>();
+    
     glen.add(7);
     glen.add(1);
     glen.add(5);
     glen.add(12);
     glen.add(3);
+    
     System.out.println( "ArrayList glen before sorting:\n" + glen );
-    pochmann.selectionSortV(glen);
+    int[] sscounters = pochmann.selectionSortV(glen);
+    System.out.println("Selection Sort COMPARE COUNTER: " + sscounters[0] + "\nSelection sort SWAP COUNTER: " + sscounters[1] + "\nSelection sort PASS COUNTER: "+sscounters[2]);
     //pochmann.insertionSortV(glen);
     //pochmann.bubbleSortV(glen);
     System.out.println( "ArrayList glen after sorting:\n" + glen );
@@ -24,29 +27,7 @@ public class Driver{
     //pochmann.insertionSortV(coco);
     //pochmann.bubbleSortV(coco);
     System.out.println( "ArrayList coco after sorting:\n" + coco );
-    // ============================================
 
-    //==========for AL-returning methods==========
-      // glen = new ArrayList<Integer>();
-      // glen.add(7);
-      // glen.add(1);
-      // glen.add(5);
-      // glen.add(12);
-      // glen.add(3);
-      // System.out.println( "ArrayList glen before sorting:\n" + glen );
-      // ArrayList glenSorted = selectionSort( glen );
-      // System.out.println( "sorted version of ArrayList glen:\n"
-      // + glenSorted );
-      // System.out.println( "ArrayList glen after sorting:\n" + glen );
-      //
-      // coco = populate( 10, 1, 1000 );
-      // System.out.println( "ArrayList coco before sorting:\n" + coco );
-      // ArrayList cocoSorted = selectionSort( coco );
-      // System.out.println( "sorted version of ArrayList coco:\n"
-      // + cocoSorted );
-      // System.out.println( "ArrayList coco after sorting:\n" + coco );
-
-  //============================================
-
+   
   }//end main
 }
